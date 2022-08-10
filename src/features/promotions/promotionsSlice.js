543bfec192404b9f225e96b4d6307023300d8c13
Skylar Foster <vskylar.foster@gmail.com>
2022-08-10 15:06:44 -0500

    Async DisplayList

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 // import { PROMOTIONS } from '../../app/shared/PROMOTIONS';
 import { baseUrl } from '../../app/shared/baseUrl';
-import { mapImageURL} from '../../utils/mapImageURL';
+import { mapImageURL } from '../../utils/mapImageURL';
 
 export const fetchPromotions = createAsyncThunk(
     'promotions/fetchPromotions',
@@ -44,5 +44,11 @@ const promotionsSlice = createSlice({
 export const promotionsReducer = promotionsSlice.reducer;
 
 export const selectFeaturedPromotion = (state) => {
-    return state.promotions.promotionsArray.find((promotion) => promotion.featured);
+    return {
+        featuredItem: state.promotions.promotionsArray.find(
+            (promotion) => promotion.featured
+        ),
+        isLoading: state.promotions.isLoading,
+        errMsg: state.promotions.errMsg
+    };
 };
